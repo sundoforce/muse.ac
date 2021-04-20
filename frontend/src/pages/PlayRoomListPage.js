51e0a7ba13682852c427b83258cab34874b2b41a
sdk <admin@sundo.kim>
2021-04-20 15:34:57 +0900

    error

--- frontend/src/pages/PlayRoomListPage.js
+++ frontend/src/pages/PlayRoomListPage.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import HeaderContainer from '../containers/common/HeaderContainer';
 import PlayRoomListContainer from '../containers/playrooms/PlayRoomListContainer';
-import {Link} from "react-router-dom";
 import PaginationContainer from "../containers/posts/PaginationContainer";
 
 const MainPage = () => {
