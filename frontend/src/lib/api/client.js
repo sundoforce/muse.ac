89e4a3d9009350c14e57018051cabac86e1d75dd
sdk <admin@sundo.kim>
2021-04-21 23:45:04 +0900

    test

--- frontend/src/lib/api/client.js
+++ frontend/src/lib/api/client.js
@@ -6,7 +6,7 @@ const client = axios.create();
   // 글로벌 설정 예시:
   
   // API 주소를 다른 곳으로 사용함
-  client.defaults.baseURL = 'http://localhost:8001/'
+  client.defaults.baseURL = 'http://192.168.55.71:8001/'
 
   // 헤더 설정
   client.defaults.headers.common['Authorization'] = 'Bearer a1b2c3d4';
