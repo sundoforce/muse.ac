af99688613cdd30177f55e31d9abf22473454907
sdk <admin@sundo.kim>
2021-04-23 20:36:04 +0900

    feat : api-domain 구입

--- src/lib/api/client.js
+++ src/lib/api/client.js
@@ -6,7 +6,7 @@ const client = axios.create();
   // 글로벌 설정 예시:
   
   // API 주소를 다른 곳으로 사용함
-  client.defaults.baseURL = 'http://localhost:8001/'
+  client.defaults.baseURL = 'https://musia.io/'
 
   // 헤더 설정
   client.defaults.headers.common['Authorization'] = 'Bearer a1b2c3d4';
